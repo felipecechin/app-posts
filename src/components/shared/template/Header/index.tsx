cc9bcd8f02c0cdccce9494bf9961d8ed9db233af
Felipe Cechin <ficechin@gmail.com>
2022-11-21 20:13:08 -0300

    updated Header component

--- src/components/shared/template/Header/index.tsx
+++ src/components/shared/template/Header/index.tsx
@@ -1,4 +1,9 @@
+import ModalContactForm from '@/components/system/ModalContactForm'
+import { useState } from 'react'
+
 function Header(): JSX.Element {
+    const [showContactModal, setShowContactModal] = useState(false)
+
     return (
         <header className='w-full bg-[#2D2D2D] px-4 py-4 fixed top-0'>
             <div className='max-w-4xl flex flex-row mx-auto text-white font-bold items-center'>
@@ -14,11 +19,17 @@ function Header(): JSX.Element {
                     </a>
                 </span>
                 <span className='text-sm sm:text-lg ml-4 sm:ml-20'>
-                    <button>
+                    <button
+                        onClick={() => setShowContactModal(true)}
+                    >
                         Contact
                     </button>
                 </span>
             </div>
+            <ModalContactForm
+                onClose={() => setShowContactModal(false)}
+                open={showContactModal}
+            />
         </header>
     )
 }
