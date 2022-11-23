f2b080e420d7b4f42c26b1d7aa00ede1f332ec91
Felipe Cechin <ficechin@gmail.com>
2022-11-22 23:37:00 -0300

    updated BigCard component styles

--- src/components/system/BigCard/index.tsx
+++ src/components/system/BigCard/index.tsx
@@ -2,6 +2,7 @@ import { AiFillForward } from 'react-icons/ai'
 import CardDescription from '../CardDescription'
 import { IPost } from '@/types/post'
 import Image from 'next/image'
+import Link from 'next/link'
 
 interface IBigCardProps {
     post: IPost
@@ -9,24 +10,32 @@ interface IBigCardProps {
 
 function BigCard({ post }: IBigCardProps): JSX.Element {
     return (
-        <div className='bg-white grid grid-cols-1 sm:grid-cols-2 h-[200px] sm:h-[350px]'>
-            <div className='hidden sm:block sm:relative'>
-                <Image
-                    alt={post.title + ' image'}
-                    fill
-                    src={post.image}
-                    style={{ objectFit: 'cover' }}
-                />
+        <Link
+            className='block hover:border hover:border-black hover:shadow-lg'
+            href={'/' + post.id}
+        >
+            <div
+                className='bg-white grid grid-cols-1 sm:grid-cols-2 h-[200px] sm:h-[350px]'
+                role={'button'}
+            >
+                <div className='hidden sm:block sm:relative'>
+                    <Image
+                        alt={post.title + ' image'}
+                        fill
+                        src={post.image}
+                        style={{ objectFit: 'cover' }}
+                    />
+                </div>
+                <div className='flex items-center relative'>
+                    <CardDescription
+                        authorName={post.author.name}
+                        content={post.content}
+                        title={post.title}
+                    />
+                    <AiFillForward className='absolute right-3 bottom-3 h-6 w-6 text-primary-brand-color' />
+                </div>
             </div>
-            <div className='flex items-center relative'>
-                <CardDescription
-                    authorName={post.author.name}
-                    content={post.content}
-                    title={post.title}
-                />
-                <AiFillForward className='absolute right-3 bottom-3 h-6 w-6 text-primary-brand-color' />
-            </div>
-        </div>
+        </Link>
     )
 }
 
