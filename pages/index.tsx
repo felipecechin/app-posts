d5e2a9596110c82ada60a1a30f4a4c8f9d1ed9b4
Felipe Cechin <ficechin@gmail.com>
2022-11-22 23:43:55 -0300

    updated initial page with SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,69 +1,24 @@
+import React, { useState } from 'react'
+
 import BigCard from '@/components/system/BigCard'
+import { GetServerSideProps } from 'next'
 import Header from '@/components/shared/template/Header'
 import { IPost } from '@/types/post'
 import InfiniteScroll from 'react-infinite-scroll-component'
+import LoadingSpinner from '@/components/shared/LoadingSpinner'
 import MainContent from '@/components/shared/template/MainContent'
-import React from 'react'
 import SmallCard from '@/components/system/SmallCard'
 
-export default function PostsPage(): JSX.Element {
+interface IPostsPageProps {
+    posts: IPost[]
+}
+
+export default function PostsPage({ posts }: IPostsPageProps): JSX.Element {
     // const [posts, setPosts] = useState(Array.from({ length: 40 }))
-    const posts: IPost[] = [
-        {
-            id: 1,
-            author: { name: 'Sheila C. Boyd' },
-            title: 'I hope the dog doesn slobber on me.',
-            content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod vestibulum erat vulputate semper. Fusce consequat lobortis mi. Donec eu orci lectus. Aenean leo arcu, sollicitudin id interdum at, sagittis egestas ante. Aliquam erat volutpat. Phasellus lobortis risus in augue bibendum, at aliquet metus consequat. Mauris venenatis dictum nisl ac congue. Etiam non quam dictum, imperdiet lectus at, gravida massa. Vivamus mattis nibh massa, vitae blandit neque tincidunt quis. In malesuada lacus a ultrices luctus. Ut lacinia enim sed libero lobortis, vitae tempus erat laoreet. Integer commodo odio pulvinar diam faucibus, vitae venenatis tellus vulputate. Sed rutrum leo nisi, a blandit elit semper id. Suspendisse at bibendum lacus, nec gravida purus. Duis vehicula faucibus diam et mattis. In bibendum nisi in leo efficitur, nec venenatis justo cursus. Nulla eget lectus diam. Nunc ex nulla, facilisis a convallis eu, dictum vitae libero. Nam rutrum commodo lectus, sed auctor nisi porttitor ac. Donec maximus, tellus nec tincidunt porttitor, lacus sapien pellentesque turpis, ut tempor enim dolor a felis. Vivamus maximus hendrerit egestas. Sed vel nunc dictum, lobortis purus at, iaculis erat. Pellentesque at quam non neque euismod pulvinar sit amet sit amet arcu. Fusce at iaculis mi, in semper nisi. Fusce lacus metus, commodo vel enim sed, posuere convallis leo. Ut auctor enim eu turpis semper, ac auctor orci efficitur. Morbi facilisis odio erat, et imperdiet elit luctus eget. Duis diam metus, bibendum sit amet placerat at, tincidunt sollicitudin velit.',
-            image: 'https://sample-posts-frontend-test.s3.amazonaws.com/post_1.jpg',
-            createdAt: '2022-04-17T16:39:02Zz',
-        },
-        {
-            id: 2,
-            author: { name: 'Noah McCann' },
-            title: 'What are you looking for?',
-            content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nisl felis, vehicula eget imperdiet vitae, scelerisque auctor magna. Maecenas ullamcorper, nunc eu sollicitudin luctus, mauris velit tincidunt enim, quis sollicitudin dolor sem ac ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla dignissim, lectus non tincidunt sollicitudin, nunc sapien sodales augue, sit amet tempor nulla orci et purus. Fusce eu finibus orci, non facilisis ligula. Sed vel libero pharetra, faucibus justo id, ullamcorper diam. Donec magna augue, gravida vel semper non, auctor eu quam. Duis id dui ullamcorper, sodales ligula ut, tempus urna. Phasellus vestibulum sapien ac justo aliquam, vel feugiat arcu pellentesque. Mauris orci lectus, ultrices eu libero a, tincidunt auctor est. Morbi pellentesque condimentum mi in convallis. Nullam molestie justo imperdiet rutrum ornare. Sed et vestibulum velit, venenatis elementum augue.',
-            image: 'https://sample-posts-frontend-test.s3.amazonaws.com/post_2.jpg',
-            createdAt: '2022-07-11T21:02:37Zz',
-        },
-        {
-            id: 3,
-            author: { name: 'Noah McCann' },
-            title: 'What are you looking for?',
-            content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nisl felis, vehicula eget imperdiet vitae, scelerisque auctor magna. Maecenas ullamcorper, nunc eu sollicitudin luctus, mauris velit tincidunt enim, quis sollicitudin dolor sem ac ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla dignissim, lectus non tincidunt sollicitudin, nunc sapien sodales augue, sit amet tempor nulla orci et purus. Fusce eu finibus orci, non facilisis ligula. Sed vel libero pharetra, faucibus justo id, ullamcorper diam. Donec magna augue, gravida vel semper non, auctor eu quam. Duis id dui ullamcorper, sodales ligula ut, tempus urna. Phasellus vestibulum sapien ac justo aliquam, vel feugiat arcu pellentesque. Mauris orci lectus, ultrices eu libero a, tincidunt auctor est. Morbi pellentesque condimentum mi in convallis. Nullam molestie justo imperdiet rutrum ornare. Sed et vestibulum velit, venenatis elementum augue.',
-            image: 'https://sample-posts-frontend-test.s3.amazonaws.com/post_2.jpg',
-            createdAt: '2022-07-11T21:02:37Zz',
-        },
-        {
-            id: 4,
-            author: { name: 'Noah McCann' },
-            title: 'What are you looking for?',
-            content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nisl felis, vehicula eget imperdiet vitae, scelerisque auctor magna. Maecenas ullamcorper, nunc eu sollicitudin luctus, mauris velit tincidunt enim, quis sollicitudin dolor sem ac ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla dignissim, lectus non tincidunt sollicitudin, nunc sapien sodales augue, sit amet tempor nulla orci et purus. Fusce eu finibus orci, non facilisis ligula. Sed vel libero pharetra, faucibus justo id, ullamcorper diam. Donec magna augue, gravida vel semper non, auctor eu quam. Duis id dui ullamcorper, sodales ligula ut, tempus urna. Phasellus vestibulum sapien ac justo aliquam, vel feugiat arcu pellentesque. Mauris orci lectus, ultrices eu libero a, tincidunt auctor est. Morbi pellentesque condimentum mi in convallis. Nullam molestie justo imperdiet rutrum ornare. Sed et vestibulum velit, venenatis elementum augue.',
-            image: 'https://sample-posts-frontend-test.s3.amazonaws.com/post_2.jpg',
-            createdAt: '2022-07-11T21:02:37Zz',
-        },
-        {
-            id: 5,
-            author: { name: 'Noah McCann' },
-            title: 'What are you looking for?',
-            content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nisl felis, vehicula eget imperdiet vitae, scelerisque auctor magna. Maecenas ullamcorper, nunc eu sollicitudin luctus, mauris velit tincidunt enim, quis sollicitudin dolor sem ac ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla dignissim, lectus non tincidunt sollicitudin, nunc sapien sodales augue, sit amet tempor nulla orci et purus. Fusce eu finibus orci, non facilisis ligula. Sed vel libero pharetra, faucibus justo id, ullamcorper diam. Donec magna augue, gravida vel semper non, auctor eu quam. Duis id dui ullamcorper, sodales ligula ut, tempus urna. Phasellus vestibulum sapien ac justo aliquam, vel feugiat arcu pellentesque. Mauris orci lectus, ultrices eu libero a, tincidunt auctor est. Morbi pellentesque condimentum mi in convallis. Nullam molestie justo imperdiet rutrum ornare. Sed et vestibulum velit, venenatis elementum augue.',
-            image: 'https://sample-posts-frontend-test.s3.amazonaws.com/post_2.jpg',
-            createdAt: '2022-07-11T21:02:37Zz',
-        },
-        {
-            id: 6,
-            author: { name: 'Noah McCann' },
-            title: 'What are you looking for?',
-            content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nisl felis, vehicula eget imperdiet vitae, scelerisque auctor magna. Maecenas ullamcorper, nunc eu sollicitudin luctus, mauris velit tincidunt enim, quis sollicitudin dolor sem ac ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla dignissim, lectus non tincidunt sollicitudin, nunc sapien sodales augue, sit amet tempor nulla orci et purus. Fusce eu finibus orci, non facilisis ligula. Sed vel libero pharetra, faucibus justo id, ullamcorper diam. Donec magna augue, gravida vel semper non, auctor eu quam. Duis id dui ullamcorper, sodales ligula ut, tempus urna. Phasellus vestibulum sapien ac justo aliquam, vel feugiat arcu pellentesque. Mauris orci lectus, ultrices eu libero a, tincidunt auctor est. Morbi pellentesque condimentum mi in convallis. Nullam molestie justo imperdiet rutrum ornare. Sed et vestibulum velit, venenatis elementum augue.',
-            image: 'https://sample-posts-frontend-test.s3.amazonaws.com/post_2.jpg',
-            createdAt: '2022-07-11T21:02:37Zz',
-        },
-    ]
+    const [postsData, setPostsData] = useState({
+        posts,
+        page: 1,
+    })
 
     const fetchData = (): void => {
         setTimeout(() => {
@@ -75,15 +30,15 @@ export default function PostsPage(): JSX.Element {
         <>
             <Header />
             <MainContent>
-                <div className='max-w-5xl mx-auto h-full'>
+                <div className='max-w-5xl mx-auto h-full mt-4'>
                     <InfiniteScroll
                         dataLength={posts.length}
                         hasMore={true}
-                        loader={<h4>Loading...</h4>}
+                        loader={<LoadingSpinner />}
                         next={() => fetchData()}
                     >
-                        <div className='grid grid-cols-1 md:grid-cols-6 gap-y-4 mt-4 overflow-auto'>
-                            {posts.map((postItem, index) => {
+                        <div className='grid grid-cols-1 md:grid-cols-6 gap-y-4 overflow-auto'>
+                            {postsData.posts.map((postItem, index) => {
                                 if (index % 6 === 0) {
                                     return (
                                         <div
@@ -163,4 +118,69 @@ export default function PostsPage(): JSX.Element {
             </MainContent>
         </>
     )
+}
+
+export const getServerSideProps: GetServerSideProps<{ posts: IPost[] }> = async () => {
+    const posts = [
+        {
+            id: 1,
+            author: { name: 'Sheila C. Boyd' },
+            title: 'I hope the dog doesn slobber on me.',
+            content:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod vestibulum erat vulputate semper. Fusce consequat lobortis mi. Donec eu orci lectus. Aenean leo arcu, sollicitudin id interdum at, sagittis egestas ante. Aliquam erat volutpat. Phasellus lobortis risus in augue bibendum, at aliquet metus consequat. Mauris venenatis dictum nisl ac congue. Etiam non quam dictum, imperdiet lectus at, gravida massa. Vivamus mattis nibh massa, vitae blandit neque tincidunt quis. In malesuada lacus a ultrices luctus. Ut lacinia enim sed libero lobortis, vitae tempus erat laoreet. Integer commodo odio pulvinar diam faucibus, vitae venenatis tellus vulputate. Sed rutrum leo nisi, a blandit elit semper id. Suspendisse at bibendum lacus, nec gravida purus. Duis vehicula faucibus diam et mattis. In bibendum nisi in leo efficitur, nec venenatis justo cursus. Nulla eget lectus diam. Nunc ex nulla, facilisis a convallis eu, dictum vitae libero. Nam rutrum commodo lectus, sed auctor nisi porttitor ac. Donec maximus, tellus nec tincidunt porttitor, lacus sapien pellentesque turpis, ut tempor enim dolor a felis. Vivamus maximus hendrerit egestas. Sed vel nunc dictum, lobortis purus at, iaculis erat. Pellentesque at quam non neque euismod pulvinar sit amet sit amet arcu. Fusce at iaculis mi, in semper nisi. Fusce lacus metus, commodo vel enim sed, posuere convallis leo. Ut auctor enim eu turpis semper, ac auctor orci efficitur. Morbi facilisis odio erat, et imperdiet elit luctus eget. Duis diam metus, bibendum sit amet placerat at, tincidunt sollicitudin velit.',
+            image: 'https://sample-posts-frontend-test.s3.amazonaws.com/post_1.jpg',
+            createdAt: '2022-04-17T16:39:02Zz',
+        },
+        {
+            id: 2,
+            author: { name: 'Noah McCann' },
+            title: 'What are you looking for?',
+            content:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nisl felis, vehicula eget imperdiet vitae, scelerisque auctor magna. Maecenas ullamcorper, nunc eu sollicitudin luctus, mauris velit tincidunt enim, quis sollicitudin dolor sem ac ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla dignissim, lectus non tincidunt sollicitudin, nunc sapien sodales augue, sit amet tempor nulla orci et purus. Fusce eu finibus orci, non facilisis ligula. Sed vel libero pharetra, faucibus justo id, ullamcorper diam. Donec magna augue, gravida vel semper non, auctor eu quam. Duis id dui ullamcorper, sodales ligula ut, tempus urna. Phasellus vestibulum sapien ac justo aliquam, vel feugiat arcu pellentesque. Mauris orci lectus, ultrices eu libero a, tincidunt auctor est. Morbi pellentesque condimentum mi in convallis. Nullam molestie justo imperdiet rutrum ornare. Sed et vestibulum velit, venenatis elementum augue.',
+            image: 'https://sample-posts-frontend-test.s3.amazonaws.com/post_2.jpg',
+            createdAt: '2022-07-11T21:02:37Zz',
+        },
+        {
+            id: 3,
+            author: { name: 'Noah McCann' },
+            title: 'What are you looking for?',
+            content:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nisl felis, vehicula eget imperdiet vitae, scelerisque auctor magna. Maecenas ullamcorper, nunc eu sollicitudin luctus, mauris velit tincidunt enim, quis sollicitudin dolor sem ac ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla dignissim, lectus non tincidunt sollicitudin, nunc sapien sodales augue, sit amet tempor nulla orci et purus. Fusce eu finibus orci, non facilisis ligula. Sed vel libero pharetra, faucibus justo id, ullamcorper diam. Donec magna augue, gravida vel semper non, auctor eu quam. Duis id dui ullamcorper, sodales ligula ut, tempus urna. Phasellus vestibulum sapien ac justo aliquam, vel feugiat arcu pellentesque. Mauris orci lectus, ultrices eu libero a, tincidunt auctor est. Morbi pellentesque condimentum mi in convallis. Nullam molestie justo imperdiet rutrum ornare. Sed et vestibulum velit, venenatis elementum augue.',
+            image: 'https://sample-posts-frontend-test.s3.amazonaws.com/post_2.jpg',
+            createdAt: '2022-07-11T21:02:37Zz',
+        },
+        {
+            id: 4,
+            author: { name: 'Noah McCann' },
+            title: 'What are you looking for?',
+            content:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nisl felis, vehicula eget imperdiet vitae, scelerisque auctor magna. Maecenas ullamcorper, nunc eu sollicitudin luctus, mauris velit tincidunt enim, quis sollicitudin dolor sem ac ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla dignissim, lectus non tincidunt sollicitudin, nunc sapien sodales augue, sit amet tempor nulla orci et purus. Fusce eu finibus orci, non facilisis ligula. Sed vel libero pharetra, faucibus justo id, ullamcorper diam. Donec magna augue, gravida vel semper non, auctor eu quam. Duis id dui ullamcorper, sodales ligula ut, tempus urna. Phasellus vestibulum sapien ac justo aliquam, vel feugiat arcu pellentesque. Mauris orci lectus, ultrices eu libero a, tincidunt auctor est. Morbi pellentesque condimentum mi in convallis. Nullam molestie justo imperdiet rutrum ornare. Sed et vestibulum velit, venenatis elementum augue.',
+            image: 'https://sample-posts-frontend-test.s3.amazonaws.com/post_2.jpg',
+            createdAt: '2022-07-11T21:02:37Zz',
+        },
+        {
+            id: 5,
+            author: { name: 'Noah McCann' },
+            title: 'What are you looking for?',
+            content:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nisl felis, vehicula eget imperdiet vitae, scelerisque auctor magna. Maecenas ullamcorper, nunc eu sollicitudin luctus, mauris velit tincidunt enim, quis sollicitudin dolor sem ac ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla dignissim, lectus non tincidunt sollicitudin, nunc sapien sodales augue, sit amet tempor nulla orci et purus. Fusce eu finibus orci, non facilisis ligula. Sed vel libero pharetra, faucibus justo id, ullamcorper diam. Donec magna augue, gravida vel semper non, auctor eu quam. Duis id dui ullamcorper, sodales ligula ut, tempus urna. Phasellus vestibulum sapien ac justo aliquam, vel feugiat arcu pellentesque. Mauris orci lectus, ultrices eu libero a, tincidunt auctor est. Morbi pellentesque condimentum mi in convallis. Nullam molestie justo imperdiet rutrum ornare. Sed et vestibulum velit, venenatis elementum augue.',
+            image: 'https://sample-posts-frontend-test.s3.amazonaws.com/post_2.jpg',
+            createdAt: '2022-07-11T21:02:37Zz',
+        },
+        {
+            id: 6,
+            author: { name: 'Noah McCann' },
+            title: 'What are you looking for?',
+            content:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nisl felis, vehicula eget imperdiet vitae, scelerisque auctor magna. Maecenas ullamcorper, nunc eu sollicitudin luctus, mauris velit tincidunt enim, quis sollicitudin dolor sem ac ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla dignissim, lectus non tincidunt sollicitudin, nunc sapien sodales augue, sit amet tempor nulla orci et purus. Fusce eu finibus orci, non facilisis ligula. Sed vel libero pharetra, faucibus justo id, ullamcorper diam. Donec magna augue, gravida vel semper non, auctor eu quam. Duis id dui ullamcorper, sodales ligula ut, tempus urna. Phasellus vestibulum sapien ac justo aliquam, vel feugiat arcu pellentesque. Mauris orci lectus, ultrices eu libero a, tincidunt auctor est. Morbi pellentesque condimentum mi in convallis. Nullam molestie justo imperdiet rutrum ornare. Sed et vestibulum velit, venenatis elementum augue.',
+            image: 'https://sample-posts-frontend-test.s3.amazonaws.com/post_2.jpg',
+            createdAt: '2022-07-11T21:02:37Zz',
+        },
+    ]
+
+    return {
+        props: {
+            posts,
+        },
+    }
 }
