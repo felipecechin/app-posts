bbda6f045f230b3838e9df3547212f2e7426b46b
Felipe Cechin <ficechin@gmail.com>
2022-11-22 23:42:26 -0300

    updated Header component

--- src/components/shared/template/Header/index.tsx
+++ src/components/shared/template/Header/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import ModalContactForm from '@/components/system/ModalContactForm'
 import { useState } from 'react'
 
@@ -8,10 +9,17 @@ function Header(): JSX.Element {
         <header className='w-full bg-[#2D2D2D] px-4 py-4 fixed top-0 z-10'>
             <div className='max-w-4xl flex flex-row mx-auto text-white font-bold items-center'>
                 <span className='flex-grow'>
-                    <h1 className='text-lg sm:text-2xl'>Rockr Blog</h1>
+                    <h1 className='text-lg sm:text-2xl'>
+                        <Link href='/'>Rockr Blog</Link>
+                    </h1>
                 </span>
                 <span>
-                    <a className='text-sm sm:text-lg'>Posts</a>
+                    <Link
+                        className='text-sm sm:text-lg'
+                        href='/'
+                    >
+                        Posts
+                    </Link>
                 </span>
                 <span className='text-sm sm:text-lg ml-4 sm:ml-20'>
                     <button onClick={() => setShowContactModal(true)}>Contact</button>
