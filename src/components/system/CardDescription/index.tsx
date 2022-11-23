a1efce0e86fcd8b004ac96411703749903ac774c
Felipe Cechin <ficechin@gmail.com>
2022-11-23 00:35:13 -0300

    updated CardDescription component

--- src/components/system/CardDescription/index.tsx
+++ src/components/system/CardDescription/index.tsx
@@ -1,4 +1,5 @@
 import styles from './styles.module.scss'
+import { useMemo } from 'react'
 
 interface ICardDescriptionProps {
     authorName: string
@@ -7,11 +8,15 @@ interface ICardDescriptionProps {
 }
 
 function CardDescription({ authorName, title, content }: ICardDescriptionProps): JSX.Element {
+    const contentWithoutTags = useMemo(() => {
+        return content.replace(/(<([^>]+)>)/gi, '')
+    }, [content])
+
     return (
         <div className='flex flex-col justify-center space-y-2 px-10'>
             <p className={styles.author}>{authorName}</p>
             <p className={styles.title}>{title}</p>
-            <p className={styles.content}>{content}</p>
+            <p className={styles.content}>{contentWithoutTags}</p>
         </div>
     )
 }
