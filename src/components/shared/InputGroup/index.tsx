d2036e87d6ba76ee51366a36c426c2e48a43424d
Felipe Cechin <ficechin@gmail.com>
2022-11-22 23:41:55 -0300

    InputGroup component

---
+++ src/components/shared/InputGroup/index.tsx
@@ -0,0 +1,19 @@
+interface IInputGroupProps {
+    label: string
+    placeholder: string
+}
+
+function InputGroup({ label, placeholder }: IInputGroupProps): JSX.Element {
+    return (
+        <div className='space-y-1'>
+            <label>{label}</label>
+            <input
+                className='w-full border border-text-color p-2 rounded-md focus:border-none focus:outline-none focus:ring-2 focus:ring-text-gradient'
+                placeholder={placeholder}
+                type='text'
+            />
+        </div>
+    )
+}
+
+export default InputGroup
